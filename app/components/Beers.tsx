8637636e3c227ece2caa6188378698761b5fc754
Brian Thornton <brianthornton@brians-mbp-2.lan>
2023-12-31 17:10:43 -0500

    updates

--- app/components/Beers.tsx
+++ app/components/Beers.tsx
@@ -16,7 +16,7 @@ const Beers: FC<IBeers> = ({ beers, type }) => {
     <div className="space-y-10 text-white">
       {beers.map((beer) => {
         return (
-          <div className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800">
+          <div key={beer.name} className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800">
             <div className="font-medium text-gray-200 group-hover:text-gray-50 ">
               <a href={beer.url}>{beer.name}</a> ({beer.abv})
             </div>
